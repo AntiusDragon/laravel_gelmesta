a55d99423c94b3c7e20ecee8a184d8411a043e1a
AntiusDragon <145384189+AntiusDragon@users.noreply.github.com>
2024-03-06 19:27:04 +0200

    first commit

---
+++ app/Http/Controllers/LabBetonasControllerController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Lab_BetonasController;
+use App\Http\Requests\StoreLab_BetonasControllerRequest;
+use App\Http\Requests\UpdateLab_BetonasControllerRequest;
+
+class LabBetonasControllerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreLab_BetonasControllerRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Lab_BetonasController $lab_BetonasController)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Lab_BetonasController $lab_BetonasController)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateLab_BetonasControllerRequest $request, Lab_BetonasController $lab_BetonasController)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Lab_BetonasController $lab_BetonasController)
+    {
+        //
+    }
+}
---
+++ app/Models/Lab_BetonasController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lab_BetonasController extends Model
+{
+    use HasFactory;
+}
